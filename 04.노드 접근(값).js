b61721e311924a3e1d59743517f51b0d24b09a02
Kim, Raymond <82295573+garlicvread@users.noreply.github.com>
2022-04-17 07:27:21 +0900

    Create 04.노드 접근(값).js

---
+++ 04.노드 접근(값).js
@@ -0,0 +1,5 @@
+// document.getElementById()를 사용하여 아이디가 apple인 요소 선택
+var apple_node = document.getElementById("apple");
+
+// firstChild.nodeValue를 사용하여 node의 첫 번째 자식 노드의 값을 `apple_pie`로 변경
+apple_node.firstChild.nodeValue = "apple_pie";
