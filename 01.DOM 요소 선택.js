78f15f86ca4dc0b51724b8fa5480accb1342e8f2
Kim, Raymond <82295573+garlicvread@users.noreply.github.com>
2022-04-16 23:14:22 +0900

    Create 01.DOM 요소 선택.js

---
+++ 01.DOM 요소 선택.js
@@ -0,0 +1,11 @@
+// 아이디가 “apple"인 요소 선택
+var selectedId = document.getElementById('apple');
+
+// 선택한 요소의 색을 red로
+selectedId.style.color = "red";
+
+// 아이디가 “banana"인 요소 선택
+var selectedId2 = document.getElementById("banana");
+
+// 선택한 요소의 색을 yellow로
+selectedId2.style.color = "yellow";
