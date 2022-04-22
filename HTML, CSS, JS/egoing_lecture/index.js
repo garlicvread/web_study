afc5066b688577aef40d613b347fb78681bf9957
Kim, Raymond <megaflops@naver.com>
2022-04-22 12:26:13 +0900

    dayNight() function updated.

--- HTML, CSS, JS/egoing_lecture/index.js
+++ HTML, CSS, JS/egoing_lecture/index.js
@@ -2,78 +2,93 @@
 // If the <input> tag, the button is clicked once again, change the background color of the <body> tag as white.
 
 
-// Flow Control.
-// function changeColor() alternative 1.
-let count = 0;
-
-function changeColor() {
-    if (count === 0) {
-        document.querySelector('body').style.backgroundColor = 'black';
-
-        // // Change the font color of the <h1> tag as white.
-        // document.querySelector('h1 a').style.color = 'white';
-
-
-        // Change the font color of the all <div ol li a> tags as white.
-        // let divs = document.querySelectorAll('div ol li a');
-        // for (let i = 0; i < divs.length; i++) {
-        //     divs[i].style.color = 'white';
-        // }
-
-        document.querySelectorAll('a').forEach(function(a) {
-            a.style.color = 'Yellow';
-        });
-
-        // Change the font color of the <body> tag as white.
-        document.querySelector('body').style.color = 'white';
-
-        // Change the value of the <input> tag as 'Day'.
-        document.querySelector('input').value = 'Day';
-
-        count = 1;
-    } else {
-        document.querySelector('body').style.backgroundColor = 'white';
-
-        // // Change the font color of the <h1> tag as blue.
-        // document.querySelector('h1 a').style.color = 'blue';
-
-        // Change the font color of the all <div ol li a> tags as black.
-        let divs = document.querySelectorAll('div ol li a');
-        for (let i = 0; i < divs.length; i++) {
-            divs[i].style.color = 'black';
-        }
-
-        document.querySelectorAll('a').forEach(function(a) {
-            a.style.color = 'blue';
-        });
-
-        // Change the font color of the <body> tag as original.
-        document.querySelector('body').style.color = 'black';
-
-        // Change the value of the <input> tag as 'Night'.
-        document.querySelector('input').value = 'Night';
-
-        count = 0;
-    }
-}
-
-
-// // Function changeColor() alternative 2 뭔가 왜 잘 안되지....
+// // Flow Control.
+// // function changeColor() alternative 1.
+// let count = 0;
+//
 // function changeColor() {
-//     if (this.value === 'Night') {
+//     if (count === 0) {
 //         document.querySelector('body').style.backgroundColor = 'black';
+//
+//         // // Change the font color of the <h1> tag as white.
+//         // document.querySelector('h1 a').style.color = 'white';
+//
+//
+//         // Change the font color of the all <div ol li a> tags as white.
+//         // let divs = document.querySelectorAll('div ol li a');
+//         // for (let i = 0; i < divs.length; i++) {
+//         //     divs[i].style.color = 'white';
+//         // }
+//
 //         document.querySelectorAll('a').forEach(function(a) {
 //             a.style.color = 'Yellow';
 //         });
+//
+//         // Change the font color of the <body> tag as white.
 //         document.querySelector('body').style.color = 'white';
+//
+//         // Change the value of the <input> tag as 'Day'.
 //         document.querySelector('input').value = 'Day';
-//     } else if (this.value === 'Day') {
+//
+//         count = 1;
+//     } else {
 //         document.querySelector('body').style.backgroundColor = 'white';
+//
+//         // // Change the font color of the <h1> tag as blue.
+//         // document.querySelector('h1 a').style.color = 'blue';
+//
+//         // Change the font color of the all <div ol li a> tags as black.
+//         let divs = document.querySelectorAll('div ol li a');
+//         for (let i = 0; i < divs.length; i++) {
+//             divs[i].style.color = 'black';
+//         }
+//
 //         document.querySelectorAll('a').forEach(function(a) {
 //             a.style.color = 'blue';
 //         });
+//
+//         // Change the font color of the <body> tag as original.
 //         document.querySelector('body').style.color = 'black';
-//         this.value = 'Night';
-//         // document.querySelector('input').value = 'Night';
+//
+//         // Change the value of the <input> tag as 'Night'.
+//         document.querySelector('input').value = 'Night';
+//
+//         count = 0;
 //     }
 // }
+
+// Function changeColor() alternative 2 ==> function separated.
+function night() {
+    document.querySelector('body').style.backgroundColor = 'black';
+    document.querySelector('body').style.color = 'white';
+
+    let aList = document.querySelectorAll('a');
+
+    for (let i = 0; i < aList.length; i++) {
+        aList[i].style.color = 'Yellow';
+    }
+    document.querySelector('input').value = 'Day';
+}
+
+function day() {
+    document.querySelector('body').style.backgroundColor = 'white';
+    document.querySelector('body').style.color = 'black';
+
+    let aList = document.querySelectorAll('a');
+    for (let i = 0; i < aList.length; i++) {
+        aList[i].style.color = 'blue';
+    }
+    document.querySelector('input').value = 'Night';
+}
+
+
+// Combine the two functions night() and day() into one function.
+function dayNight() {
+    if (this.value === 'Night') {
+        night();
+        this.value = 'Day';
+    } else {
+        day();
+        this.value = 'Night';
+    }
+}
