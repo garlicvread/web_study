547acde196c9f1348ae7edd79189d07794a4f173
Kim, Raymond <megaflops@naver.com>
2022-04-24 10:35:23 +0900

    웹페이지 전환 코드 w/ JS(렌더링)

--- HTML, CSS, JS/webNotes/navTag.js
+++ HTML, CSS, JS/webNotes/navTag.js
@@ -12,18 +12,38 @@
 
 
 // nav function variations 2. Using arrow function.
-let nav = () => {
-    let navTag = '';
+let nav = () =>  {
+    let navTag = ''; // A tag to append li tags dynamically.
 
     for (let i = 0; i < topics.length; i++) {
-        navTagl += `<li><a href="/read/${topics[i].id}">${topics[i].title}</a></li>`;
+        navTag = navTag + `
+            <li>
+                <a href="/read/${topics[i].id}" id="${topics[i].id}" onclick="
+                    event.preventDefault();
+                    console.log(event.target.id);
+                    
+                    for(let i = 0; i < topics.length; i++){
+                        if(Number(topics[i].id) === Number(event.target.id)){
+                            // event.target.id is the id of the clicked link.
+                            
+                            document.querySelector('article').innerHTML = '<h2>${topics[i].title}</h2>${topics[i].body}';
+                            // Replacing the article element with the clicked link's article.
+                            
+                            break;
+                        }
+                    }
+                ">
+                    ${topics[i].title}
+                </a>
+            </li>`;
     }
 
     document.querySelector('nav ol').innerHTML = navTag;
     console.log(navTag);
 }
 
+nav();
 
 // nav function variations 3: Using map() function.
 // map() function must receive a callback function as the first argument.
-// The callback function returns the current argument to process in the array to the first argument of map().
+// The  callback function returns the current argument to process in the array to the first argument of map().
